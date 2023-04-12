67014687558cd60b5a606a05297885f42261b754
Khalid Hussein <khalhussi@gmail.com>
2023-04-12 14:35:01 +0100

    MVP almost completed

--- src/main/java/AirPort.java
+++ src/main/java/AirPort.java
@@ -30,7 +30,11 @@ public class AirPort {
     public void displayFlights(){
         for(Airplane airplane : this.airplanes){
             System.out.println(airplane.getDestination() + " " + airplane.getID());
-
+        }
+    }
+    public void addPassengerToFlights(Airplane airplane) {
+        for (Passenger passenger : this.passengers) {
+            System.out.println(passenger + " has been added to" + airplane);
         }
     }
 
@@ -38,17 +42,45 @@ public class AirPort {
 
 
 
-    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Airplane airplane1;
         Airplane airplane2;
         Airplane airplane3;
+        Passenger passenger1;
+        Passenger passenger2;
+        Passenger passenger3;
+
+        passenger1 = new Passenger("Bob", 63, "passenger1@gmail.com");
+        passenger2 = new Passenger("Joe", 10, "passenger2@Gmail.com");
+        passenger3 = new Passenger("Sam", 30, "passenger3@gmail.com");
 
         airplane1 = new Airplane("France", 32);
         airplane2 = new Airplane("Peru", 40);
         airplane3 = new Airplane("Japan", 60);
-
         AirPort airPort = new AirPort();
+        airPort.addFlights(airplane1);
+        airPort.addFlights(airplane2);
+        airPort.addFlights(airplane3);
+        airPort.addPassengers(passenger1);
+        airPort.addPassengers(passenger2);
+        airPort.addPassengers(passenger3);
 
 
 
@@ -61,7 +93,7 @@ public class AirPort {
             String input = scanner.nextLine();
             try{
                 if(input.equals(answer)) {
-                  airPort.displayFlights();
+                    airPort.displayFlights();
                     break;
                 }
                 else {
@@ -75,6 +107,46 @@ public class AirPort {
             }
 
         }
+
+        int response1 = 32;
+        int response2 = 40;
+        int response3 = 60;
+        System.out.println("Please enter the flight ID number u wish to be booked onto");
+        while(true){
+            String input2 = scanner.nextLine();
+            try{
+                if(input2.equals(response1)){
+                    airPort.addPassengerToFlights(airplane1);
+                } if(input2.equals(response2)){
+                    airPort.addPassengerToFlights(airplane2);
+                } if(input2.equals(response3)){
+                    airPort.addPassengerToFlights(airplane3);
+                } else {
+                    System.out.println("Have a nice day sir/madam");
+                    break;
+                }
+            }
+            catch(NumberFormatException e) {
+                System.out.println("sorry please type a number");
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
--- src/main/java/Airplane.java
+++ src/main/java/Airplane.java
@@ -17,6 +17,7 @@ public class Airplane {
 
 
 
+
     public String getDestination() {
         return destination;
     }
